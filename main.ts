3e6a57320a11b75a4b53f34f5f0a7c84cb2941d2
Muhammad Jalal Khanzada <160409122+MJalalkhanzada@users.noreply.github.com>
2024-03-31 23:56:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers : {
+    numOne: number,
+    numTwo: number,
+    operator: string,
+} = await inquirer.prompt ([
+    {
+        type : "number",
+        name : "numOne",
+        message : (chalk.yellow("Enter your first number"))
+    },
+    {   
+        type : "number",
+        name : "numTwo",
+        message : (chalk.blue.bold("Enter your second number"))
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Enter your operator",
+        choices :["+", "-", "*", "/", "%"]}
+])
+const {numOne, numTwo, operator } = answers;
+
+let result
+switch (operator) {
+    case "+":
+        result = numOne + numTwo; 
+        break;
+    case "-":
+        result = numOne - numTwo;
+        break;
+    case "*":
+        result = numOne * numTwo;
+        break;
+    case "/":
+        result = numOne / numTwo;
+        break;
+    case "%":
+        result = numOne % numTwo;
+        break;
+        
+        default:
+        console.log("Invalid Operator");
+    }
+    console.log(`${numOne} ${operator} ${numTwo} = ${result}`);
+    
+    
